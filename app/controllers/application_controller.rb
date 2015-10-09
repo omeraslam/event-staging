56a483ecc7bf7c461f04914a53ac0eb03c18f78c
Richard Saethang <richard.saethang@gmail.com>
2015-10-08 17:46:03 -0700

    Closes #20 - create white page class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper :all
+
 end
+
+def current_root_class
+  'class=pages' if controller_name == "pages"
+end 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
 
   end
     def home
+        
   end
 
   def terms
