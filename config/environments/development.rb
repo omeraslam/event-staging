02f3fee9df1cf43ee465f1d0a03103ef5dd71f6c
Richard Saethang <richard.saethang@gmail.com>
2015-10-27 19:09:14 -0700

    ssl update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+
+  #force ssl on pages
+  config.force_ssl = false
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
