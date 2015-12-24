3b7b5d6be56d272a62b3c4436a1142944afd961d
mjbushy <mjbushy@gmail.com>
2015-12-23 16:16:13 -0800

    updated themes

---
+++ app/assets/javascripts/themes.js
@@ -0,0 +1,34 @@
+{
+   "themes":[
+      {
+         "name":"Cityscape",
+         "slug":"cityscape",
+         "active":"true"
+      },
+      {
+         "name":"Get Loud",
+         "slug":"getloud",
+         "active":"true"
+      },
+      {
+         "name":"Epic",
+         "slug":"epic",
+         "active":"true"
+      },
+      {
+         "name":"Gallery",
+         "slug":"gallery",
+         "active":"true"
+      },
+      {
+         "name":"Celebrate",
+         "slug":"celebrate",
+         "active":"true"
+      },
+      {
+         "name":"The Minimalist",
+         "slug":"minimalist",
+         "active":"true"
+      }
+   ]
+}
