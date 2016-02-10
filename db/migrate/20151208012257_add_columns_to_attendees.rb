4b20af292589d37d9e2973fe18232f3de62137b7
Richard Saethang <richard.saethang@gmail.com>
2016-02-10 08:05:40 -0800

    remove dup migration

--- db/migrate/20151208012257_add_columns_to_attendees.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToAttendees < ActiveRecord::Migration
-    def change
-        add_column :attendees, :event_id, :integer
-        add_column :attendees, :user_id, :string
-    end
-end
-
