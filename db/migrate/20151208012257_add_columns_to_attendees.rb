c9777b4942b510130813f80d743b53121b6d599a
Richard Saethang <richard.saethang@gmail.com>
2016-02-10 07:57:24 -0800

    remove dup column on attendees

--- db/migrate/20151208012257_add_columns_to_attendees.rb
+++ db/migrate/20151208012257_add_columns_to_attendees.rb
@@ -1,6 +1,5 @@
 class AddColumnsToAttendees < ActiveRecord::Migration
     def change
-        add_column :attendees, :attending, :boolean
         add_column :attendees, :event_id, :integer
         add_column :attendees, :user_id, :string
     end
