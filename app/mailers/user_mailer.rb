09c63c4401b95f0f6016610e6218915f58d2b5d5
Richard Saethang <richard.saethang@gmail.com>
2016-02-29 09:47:54 -0800

    validation on sign in forms

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ActionMailer::Base
 
     def welcome_email(user)
         @user = user
-        @url = ENV['SITE_NAME'] + '/create'
+        @url = 'https://eventcreate.com/create'
         mail(to: @user.email, subject: 'Welcome to EventCreate')
     end
 
