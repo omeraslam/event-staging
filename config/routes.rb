cfc99fa32b9373509eb60d6d15f65fd1c9de7868
Richard Saethang <richard.saethang@gmail.com>
2016-08-10 18:57:42 +1000

    logic group subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   get '/thank-you',  to: 'payments#thankyou', :as => :thankyou
   get '/upgrade' => 'payments#upgrade', :as => :upgrade
   get '/cancel' => 'payments#cancel', :as => :cancel
-  get '', to: 'events#home', constraints: lambda { |r| r.subdomain.present? && r.subdomain != 'www' && r.domain == 'www.markbushyphotography.com' }, :as => :events_subdomain
+  get '', to: 'events#home', constraints: lambda { |r| (r.subdomain.present? && r.subdomain != 'www') || r.domain == 'www.markbushyphotography.com' }, :as => :events_subdomain
   get '/users/:id/events/index', to: 'events#home', :as => :events_home
  
 
