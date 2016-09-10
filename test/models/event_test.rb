1023c6a3d6e5ff5a8225a4bd6520aadd99c66389
Richard Saethang <richard.saethang@gmail.com>
2016-09-10 09:15:47 -0400

    event testing

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -4,4 +4,10 @@ class EventTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+    test "should not save event without name" do
+        event = Event.new
+        assert_not event.save,  "Saved the event without a title"
+        # assert true
+    end
 end
