ffa7a857133f3179ad8d7dd2e8da8cafa415ba59
Richard Saethang <richard.saethang@gmail.com>
2016-10-14 02:28:01 -0700

    hide config ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
   #force ssl on pages
-  config.force_ssl = false
+  #config.force_ssl = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
