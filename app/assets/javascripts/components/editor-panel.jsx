ca743d3ebc7b09b74dc2889896f110f130e0dc0e
Richard Saethang <richard.saethang@gmail.com>
2016-10-17 21:52:58 -0700

    formatting date to form

--- app/assets/javascripts/components/editor-panel.jsx
+++ app/assets/javascripts/components/editor-panel.jsx
@@ -17,10 +17,27 @@ var EditorPanel = React.createClass({
     //     current_selection: nextProps.current_selection
     //   });
     // },
+    // 
+    handleUpdate: function(e) {
+        alert('handle update');
+        e.preventDefault();
+        this.props.current_selection["stop_date"] = moment(this.props.current_selection["stop_date"], "MM/DD/YYYY");
+
+        alert(moment(this.props.current_selection["stop_date"], "MM/DD/YYYY"));
+
+        $.ajax({
+            method: 'PUT',
+            url: '/tickets/' + this.props.current_selection.id,
+            data: {"ticket" : this.props.current_selection},
+            dataType: 'JSON',
+            success: function() {
+                alert('success');
+               // this.props.handleDeleteEvent(this.props.event)
+            }.bind(this)
+        });
+    },
 
     render: function() {
-   
-
         return (
             <div className="editor-panel">
             
@@ -29,26 +46,26 @@ var EditorPanel = React.createClass({
                 <form>
                     <div className="input-group">
                         <label> Ticket Name</label>
-                        <input type="text" value={this.props.current_selection.title} />
+                        <input type="text" defaultValue={this.props.current_selection.title} />
                     </div>
                     <div className="input-group">
                         <label> Ticket Active</label>
-                        <input type="text" value={this.props.current_selection.active} />
+                        <input type="text" defaultValue={this.props.current_selection.active} />
                     </div>
                     <div className="input-group">
                         <label>Quantity Available</label>
-                        <input type="text" value={this.props.current_selection.ticket_limit} />
+                        <input type="text" defaultValue={this.props.current_selection.ticket_limit} />
                     </div>
                     <div className="input-group">
                         <label>Buy Limit</label>
-                        <input type="text" value={this.props.current_selection.buy_limit} />
+                        <input type="text" defaultValue={this.props.current_selection.buy_limit} />
                     </div>
                     <div className="input-group">
                         <label>Registration closes on</label>
-                        <input type="text" value={this.props.current_selection.stop_date} />
+                        <input type="text" defaultValue={this.props.current_selection.stop_date} />
                     </div>
                     <div className="input-group">
-                        <button className="btn btn-primary">Update</button>
+                        <button onClick={this.handleUpdate} className="btn btn-primary">Update</button>
                     </div>
                 </form>
             </div>
