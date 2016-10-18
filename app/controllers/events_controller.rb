9edec62aa625c06ead39e071e060cbeb6a779d8a
Richard Saethang <richard.saethang@gmail.com>
2016-10-17 23:58:14 -0700

    check for domains

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1088,10 +1088,9 @@ def show
       @account = nil
     end
      
-    # if request.domain != ENV['SITE_URL']
-    #   @event = Event.find_by_domain(request.host) or not_found
-    # els
-    if params[:slug].nil?
+    if request.domain != ENV['SITE_URL'].to_s
+      @event = Event.find_by_domain(request.host) or not_found
+    elsif params[:slug].nil?
       @event = Event.find_by_slug(request.subdomain) or not_found
     else
       @event = Event.find_by_slug(params[:slug]) or not_found
